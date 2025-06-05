faf2e13a4cc511a9a50a8c8c6704d2518f8b6cc5
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-06-05 20:16:24 -0300

    Save form state to make inputs editable

--- src/components/PersonalInformationForm.jsx
+++ src/components/PersonalInformationForm.jsx
@@ -1,4 +1,5 @@
 import FormField from "./FormField.jsx";
+import { useState } from "react";
 
 export default function PersonalInformationForm({
   handleSave,
@@ -9,26 +10,50 @@ export default function PersonalInformationForm({
     name
     email
     phone number
-   */
+  */
+
+  const [formData, setFormData] = useState({
+    name: null,
+    email: null,
+    phone: null,
+  });
+
+  function handleChange(event) {
+    const key = event.target.name;
+    const value = event.target.value;
+
+    setFormData({
+      ...formData,
+      [key]: value,
+    });
+  }
+
   return (
     <form>
       <fieldset>
-      <legend>Personal Information</legend>
-      <FormField name="name" label="Name" value={personalInformation.name} />
-      <FormField
-        name="email"
-        label="Email"
-        value={personalInformation.email}
-        type="email"
-        placeholder="email@example.com"
-      />
-      <FormField
-        name="phone"
-        label="Phone number"
-        value={personalInformation.phone}
-        type="tel"
-        placeholder="555-555-555"
-      />
+        <legend>Personal Information</legend>
+        <FormField
+          name="name"
+          label="Name"
+          value={formData.name || personalInformation.name}
+          onChange={handleChange}
+        />
+        <FormField
+          name="email"
+          label="Email"
+          value={formData.email || personalInformation.email}
+          type="email"
+          placeholder="email@example.com"
+          onChange={handleChange}
+        />
+        <FormField
+          name="phone"
+          label="Phone number"
+          value={formData.phone || personalInformation.phone}
+          type="tel"
+          placeholder="555-555-555"
+          onChange={handleChange}
+        />
         <button type="button" onClick={handleSave}>
           Save
         </button>
